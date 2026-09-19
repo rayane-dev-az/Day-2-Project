f42074cddc0715f258ca1b4b6a51be6be854a829
rayane-dev-az <azmourayane@gmail.com>
2026-09-19 22:19:14 +0200

    Add files via upload

---
+++ rayane.py
@@ -0,0 +1,9 @@
+fristname = "xxx"
+lastname = "xxx"
+full_name = fristname + " " +lastname
+name_length  = len(full_name)
+level = 2
+power = name_length ** level
+print("Name:", full_name)
+print("Letters:", name_length)
+print("Power:", power)
